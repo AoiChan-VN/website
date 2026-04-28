e44dd53b162425c2c8d488e36098f0edac264df0
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-29 02:18:23 +0700

    Refactor ProjectCard props and update template
    Updated ProjectCard to use id and type props instead of tech and link.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,17 +2,14 @@ import Component from '../core/Component.js';
 
 export default class ProjectCard extends Component {
     template() {
-        const { title, desc, tech, image, link } = this.props;
+        const { id, title, desc, image, type } = this.props;
         return `
             <div class="card">
-                <img src="${image}" alt="${title}" class="card-img">
+                <img src="${image}" class="card-img">
                 <div class="card-content">
                     <h3>${title}</h3>
                     <p>${desc}</p>
-                    <div class="tech-stack">
-                        ${tech.map(t => `<span class="badge">${t}</span>`).join('')}
-                    </div>
-                    <a href="${link}" class="btn-link">Xem chi tiết</a>
+                    <a href="#/${type}/${id}" class="btn-link">Xem chi tiết →</a>
                 </div>
             </div>
         `;
