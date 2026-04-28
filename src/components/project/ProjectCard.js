302f9ffef5aabca20861090e35790dc91a3b7548
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-29 06:55:21 +0700

    Add ProjectCard component for project display

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -1,1 +1,20 @@
+import Component from '../../core/Component.js';
+import { globalStore } from '../../store/index.js';
 
+export default class ProjectCard extends Component {
+  template() {
+    const { item, type } = this.$props;
+    return `
+      <div class="card" data-id="${item.id}">
+        <div class="card-info">
+          <h3>${item.name}</h3>
+          <p>${type === 'plugin' ? `Version: ${item.version}` : `Res: ${item.resolution}`}</p>
+        </div>
+        <div class="card-actions">
+          <button class="btn-detail" data-id="${item.id}">Chi tiết</button>
+          <button class="btn-delete" data-id="${item.id}">Xóa</button>
+        </div>
+      </div>
+    `;
+  }
+}
