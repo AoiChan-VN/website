5cbc77fad4dc16e376c74c70b38ec356791c880f
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-29 07:34:19 +0700

    Add Navbar component initialization in app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import Router from './core/Router.js';
 import DashboardPage from './pages/DashboardPage.js';
+import Navbar from './components/shared/Navbar.js';
 import Modal from './components/shared/Modal.js';
 import { store } from './store/index.js';
 
 // 1. Khởi tạo các thanh phần Global
 new Modal(document.querySelector('#modal-root'));
+new Navbar(document.querySelector('#nav-root'));
 
 // 2. Khởi tạo Toast Container (Tự re-render khi store.toasts đổi)
 const renderToast = () => {
