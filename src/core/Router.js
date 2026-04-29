42eb6418a1e0b239024d95d2ccb8e90ff83d477e
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-29 09:08:03 +0700

    Add files via upload

--- src/core/Router.js
+++ src/core/Router.js
@@ -1,1 +1,22 @@
-
+export default class Router {
+    constructor(routes) {
+        this.routes = routes;
+        this.init();
+    }
+    init() {
+        window.addEventListener('popstate', () => this.render());
+        document.body.addEventListener('click', e => {
+            if (e.target.matches('[data-link]')) {
+                e.preventDefault();
+                history.pushState(null, null, e.target.href);
+                this.render();
+            }
+        });
+        this.render();
+    }
+    render() {
+        const route = this.routes[location.pathname] || this.routes['/404'];
+        document.querySelector('#app').innerHTML = route.template();
+    }
+}
+ 
