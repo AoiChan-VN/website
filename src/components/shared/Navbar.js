d4135b6d4b6602d2e7377b3fa4ec155ef73c09e3
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-29 07:47:27 +0700

    Refactor Navbar component structure and styling

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -3,15 +3,13 @@ import Component from '../../core/Component.js';
 export default class Navbar extends Component {
   template() {
     return `
-      <nav class="main-nav">
-        <div class="nav-logo">MC-ADMIN</div>
-        <ul class="nav-links">
-          <li><a href="/" data-link>Dashboard</a></li>
-          <li><a href="/plugins" data-link>Plugins</a></li>
-          <li><a href="/resources" data-link>Resources</a></li>
-        </ul>
-        <div class="server-badge">Server: <span class="status-dot"></span> Online</div>
+      <nav class="nav-bar">
+        <div class="logo">MC-SPA</div>
+        <div class="links">
+          <a href="/" data-link>Dashboard</a>
+          <a href="/plugins" data-link>Plugins</a>
+          <a href="/resources" data-link>Resources</a>
+        </div>
       </nav>`;
   }
 }
-
