7ffa5520925cb8c262c469224ba802c1d0e0e981
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-29 07:03:32 +0700

    Implement ResourcePage component with project cards

--- src/pages/ResourcePage.js
+++ src/pages/ResourcePage.js
@@ -1,1 +1,28 @@
+import Component from '../core/Component.js';
+import ProjectCard from '../components/project/ProjectCard.js';
+import { resourceStore } from '../store/index.js';
 
+export default class ResourcePage extends Component {
+  setup() {
+    resourceStore.subscribe(() => this.render());
+  }
+
+  template() {
+    const { packs } = resourceStore.state;
+    return `
+      <div class="page-container">
+        <h1>Resource Pack Manager</h1>
+        <div class="resource-grid">
+          ${packs.map(pack => `<div class="pack-item" data-id="${pack.id}"></div>`).join('')}
+        </div>
+      </div>
+    `;
+  }
+
+  mounted() {
+    const { packs } = resourceStore.state;
+    this.$target.querySelectorAll('.pack-item').forEach((el, index) => {
+      new ProjectCard(el, { item: packs[index], type: 'resource' });
+    });
+  }
+}
