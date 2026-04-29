09574e19a1707a7411fd937f5b05c5109444f5f3
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-29 08:14:12 +0700

    Delete src/utils directory

--- src/utils/Observer.js
+++
@@ -1,6 +0,0 @@
-export default class Observer {
-  constructor() { this.subscribers = new Set(); }
-  subscribe(fn) { this.subscribers.add(fn); }
-  unsubscribe(fn) { this.subscribers.delete(fn); }
-  notify(data) { this.subscribers.forEach(fn => fn(data)); }
-}
