cd477218699449dafe3d90c104cc7dd2fa5ce890
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-04 09:01:06 +0700

    Update and rename db.ts to store.js

---
+++ src/data/store.js
@@ -0,0 +1,14 @@
+export const MyResources = {
+  plugins: [
+    {
+      id: 1,
+      name: "Anti-Hack Pro",
+      thumbnail: "https://dot.com",
+      price: "Premium",
+      downloadId: "aHR0cHM6Ly9maWxlLXNoYXJlLmNvbS9hbnRpLWhhY2suemlw" // Link đã mã hóa
+    }
+  ],
+  videos: [
+    { id: "yt-01", trailer: "video_id_here", type: "youtube" }
+  ]
+};
