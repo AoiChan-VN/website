67d16d3c4d09805fd99cfc2394a060eee42e8b2f
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-05 04:15:34 +0700

    Add resourcesDB with initial resource entry

---
+++ src/data/resources.js
@@ -0,0 +1,10 @@
+export const resourcesDB = [
+    {
+        id: "res-001",
+        name: "Ultra Realistic Texture",
+        resolution: "512x",
+        fileSize: "1.2GB",
+        downloadPath: "/public/downloads/res/ultra-pack.zip"
+    }
+];
+ 
