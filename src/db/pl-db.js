884d320e7dfb387ba4326063db3a0c3ca1509b38
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-05 19:32:30 +0700

    Update and rename .nojekyll to src/db/pl-db.js

---
+++ src/db/pl-db.js
@@ -0,0 +1,14 @@
+/** 
+ * @database Plugins
+ * @version 1.1.0 
+ */
+const DATABASE_PLUGINS = {
+    tag: "STABLE_2024",
+    author: "AoiChan",
+    data: [
+        { id: "PL_01", name: "Security Shield", ver: "1.0", path: "/plugins/sec" },
+        { id: "PL_02", name: "Auto SEO", ver: "2.5", path: "/plugins/seo" }
+    ]
+};
+export default DATABASE_PLUGINS;
+
