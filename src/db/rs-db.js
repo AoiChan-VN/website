45042bff0cb212bf2d0fed10f90f8557a529dc93
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-05 22:35:27 +0700

    Create rs-db.js for resource database
    Added a new resource database with items and metadata.

---
+++ src/db/rs-db.js
@@ -0,0 +1,14 @@
+/**
+ * @database Resources
+ * @version 2.0.1
+ */
+const DATABASE_RESOURCE = {
+    tag: "RESOURCE_STABLE",
+    author: "AoiChan",
+    items: [
+        { id: "rs-01", name: "UI Kit Photoshop", type: "PSD", size: "120MB", link: "#" },
+        { id: "rs-02", name: "Custom Icons Pack", type: "PNG/SVG", size: "15MB", link: "#" }
+    ]
+};
+export default DATABASE_RESOURCE;
+ 
