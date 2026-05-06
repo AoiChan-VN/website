51d0c1e13bcbdebcd2f88fa42e828dda5e2ee237
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-07 03:09:17 +0700

    Update rs-db.js

--- src/db/rs-db.js
+++ src/db/rs-db.js
@@ -1,14 +1,20 @@
-/**
- * @database Resources
- * @version 2.0.1
- */
-const DATABASE_RESOURCE = {
-    tag: "RESOURCE_STABLE",
-    author: "AoiChan",
-    items: [
-        { id: "rs-01", name: "UI Kit Photoshop", type: "PSD", size: "120MB", link: "#" },
-        { id: "rs-02", name: "Custom Icons Pack", type: "PNG/SVG", size: "15MB", link: "#" }
-    ]
-};
-export default DATABASE_RESOURCE;
- 
+export const RESOURCE_DATA = [
+    {
+        id: "rs-001",
+        name: "Aoi-Texture-Ultra",
+        version: "1.20.x",
+        tag: "new",
+        size: "256x",
+        image: "../assets/img/resource/ultra.jpg",
+        link: "#"
+    },
+    {
+        id: "rs-002",
+        name: "Legacy UI Pack",
+        version: "1.12 - 1.16",
+        tag: "old",
+        size: "Default",
+        image: "../assets/img/resource/legacy.jpg",
+        link: "#"
+    }
+];
