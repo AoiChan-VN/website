2468bfb1054526e1d1cb39bc3eb52f9780a5775c
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-07 22:02:17 +0700

    Add application bootstrapping functionality

---
+++ assets/js/core/app.boot.js
@@ -0,0 +1,30 @@
+import { initializeTheme } from "./theme.js";
+import { initializeRouter } from "./router.js";
+import { initializeCache } from "./cache.js";
+import { initializeSEO } from "./seo.js";
+
+export function bootApplication() {
+
+  initializeTheme();
+
+  initializeCache();
+
+  initializeSEO();
+
+  initializeRouter();
+
+  registerServiceWorker();
+
+}
+
+function registerServiceWorker() {
+
+  if ("serviceWorker" in navigator) {
+
+    navigator.serviceWorker.register(
+      "/workers/service-worker.js"
+    );
+
+  }
+
+}
