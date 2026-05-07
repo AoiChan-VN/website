8a8940821aae5bc1333b471dafd7f8e91968546e
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-08 04:40:46 +0700

    Add searchTemplate function for search input

---
+++ assets/js/modules/search/search.template.js
@@ -0,0 +1,19 @@
+export function searchTemplate() {
+
+  return `
+
+    <div class="search-container">
+
+      <input
+        type="search"
+        id="global-search-input"
+        class="search-input"
+        placeholder="Search plugins, repositories..."
+        autocomplete="off"
+      />
+
+    </div>
+
+  `;
+
+}
