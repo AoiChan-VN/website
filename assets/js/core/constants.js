a7594306a2a1eefdc12d9a9ab167123d6c000750
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-07 22:37:06 +0700

    Add constants for app configuration and routes

---
+++ assets/js/core/constants.js
@@ -0,0 +1,40 @@
+/**
+ * Project: AoiChan Portfolio
+ * Author: AoiChan
+ * License: MIT
+ */
+
+export const APP_NAME =
+  "AoiChan Portfolio";
+
+export const APP_VERSION =
+  "2.0.0";
+
+export const AUTHOR =
+  "AoiChan";
+
+export const STORAGE_KEYS = {
+
+  THEME:
+    "aoichan-theme",
+
+  CACHE:
+    "aoichan-cache"
+
+};
+
+export const ROUTES = {
+
+  HOME:
+    "/",
+
+  PLUGINS:
+    "/plugins",
+
+  REPOSITORIES:
+    "/repositories",
+
+  DASHBOARD:
+    "/dashboard"
+
+}; 
