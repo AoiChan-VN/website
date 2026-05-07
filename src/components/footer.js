9aac6045e494a7b5b7c6ca0a71dc76e18528424b
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-07 11:50:22 +0700

    Delete src directory

--- src/components/footer.js
+++
@@ -1,9 +0,0 @@
-export const Footer = {
-    render: () => `
-    <footer class="footer-main">
-        <div class="container text-center">
-            <p>&copy; 2024-2026 <strong>AoiChan</strong>. All Rights Reserved.</p>
-            <div class="license-tag">License: Exclusive Authorship</div>
-        </div>
-    </footer>`
-};
