f8de3ce3c2f8bbea56a82efd20ae079ef449eded
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-08 06:47:32 +0700

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+/**
+ * Project: AoiChan Portfolio
+ * Author: AoiChan
+ * License: MIT
+ */
+
+import { initializeRouter }
+from "./core/router.js";
+
+import { initializeTheme }
+from "./core/theme.js";
+
+import { initializeSEO }
+from "./core/seo.js";
+
+import { preloadAssets }
+from "./core/preloader.js";
+
+import { initializeCache }
+from "./core/cache.js";
+
+async function bootstrap() {
+
+  initializeTheme();
+
+  initializeSEO();
+
+  initializeCache();
+
+  preloadAssets();
+
+  initializeRouter();
+
+}
+
+bootstrap(); 
