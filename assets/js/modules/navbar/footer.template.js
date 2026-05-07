a559f1c55a3cb7d576d18daae0addba84f1fc33a
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-07 22:45:18 +0700

    Create footer.template.js

---
+++ assets/js/modules/navbar/footer.template.js
@@ -0,0 +1,19 @@
+export function footerTemplate() {
+
+  return `
+
+    <div class="footer-container">
+
+      <p class="footer-copyright">
+        © 2026 AoiChan
+      </p>
+
+      <p class="footer-version">
+        Portfolio System v2.0
+      </p>
+
+    </div>
+
+  `;
+
+}
