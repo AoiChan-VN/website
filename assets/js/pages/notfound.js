6d10cd423575c9c9a525c492219b1a237e4100a6
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-09 01:14:12 +0700

    Add renderNotFoundPage function for 404 page

---
+++ assets/js/pages/notfound.js
@@ -0,0 +1,13 @@
+export function renderNotFoundPage(root) {
+
+    root.innerHTML = `
+        <section class="container">
+
+            <h1>404</h1>
+
+            <p>Page not found.</p>
+
+        </section>
+    `;
+
+} 
