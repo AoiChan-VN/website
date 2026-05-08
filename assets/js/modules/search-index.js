31f9fe7cf9e4eb7074b4157d67452f08343b6726
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-09 04:00:02 +0700

    Add buildSearchIndex function for search indexing

---
+++ assets/js/modules/search-index.js
@@ -0,0 +1,32 @@
+import { sanitize }
+from './sanitize.js';
+
+export function buildSearchIndex(
+    posts
+) {
+
+    return posts.map(post => {
+
+        const index = [
+
+            post.title,
+            post.description,
+            post.content,
+            ...(post.tags || [])
+
+        ]
+            .join(' ')
+            .toLowerCase();
+
+        return {
+
+            post,
+
+            searchable:
+                sanitize(index)
+
+        };
+
+    });
+
+} 
