468a40ffdd2362c7c01787de4910aa7653a781ee
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-09 03:58:51 +0700

    Invoke runCleanup in the router function
    Add runCleanup function call to router

--- assets/js/core/router.js
+++ assets/js/core/router.js
@@ -1,11 +1,16 @@
 import { renderHomePage } from '../pages/home.js';
 import { renderPostPage } from '../pages/post.js';
 import { renderNotFoundPage } from '../pages/notfound.js';
+import { runCleanup } from './js/modules/lifecycle.js';
 
 export async function router() {
 
+    runCleanup();
+
     const app =
         document.querySelector('#app');
+
+    app.innerHTML = '';
 
     const hash =
         location.hash.slice(1) || '/';
