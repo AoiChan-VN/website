f9831915825fc26dcffd5e6d4aa433aa461d3446
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-09 23:36:27 +0700

    Refactor fetchJSON to include caching functionality

--- assets/js/core/loader.js
+++ assets/js/core/loader.js
@@ -1,14 +1,32 @@
-async function fetchJSON(path) {
+import {
+    hasCache,
+    getCache,
+    setCache
+} from './cache.js'
+
+export async function fetchJSON(path) {
+
+    if (hasCache(path)) {
+
+        return getCache(path)
+
+    }
 
     const response = await fetch(path)
 
     if (!response.ok) {
 
-        throw new Error(`Failed to fetch: ${path}`)
+        throw new Error(
+            `Fetch failed: ${path}`
+        )
 
     }
 
-    return response.json()
+    const data = await response.json()
+
+    setCache(path, data)
+
+    return data
 
 }
 
