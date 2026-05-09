744dfe4a3a94dc25806e6869aa16d293c7ab93fb
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-09 08:17:16 +0700

    Add accessibility initialization to app.js

--- assets/js/core/app.js
+++ assets/js/core/app.js
@@ -2,9 +2,17 @@ import { router } from './router.js';
 
 import { initializeTheme } from '../modules/theme.js';
 
+import { initializeAccessibility } from '../modules/accessibility.js';
+
+import { initializeInteractions } from '../modules/interaction.js';
+
 export function initApp() {
 
     initializeTheme();
+
+    initializeAccessibility();
+
+    initializeInteractions();
 
     window.addEventListener(
         'hashchange',
