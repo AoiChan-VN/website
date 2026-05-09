7b5288e5cfafbce2a435f2d0e158b5f6831836e8
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-09 23:42:12 +0700

    Add NotFoundPage component for 404 error handling

---
+++ assets/js/pages/not-found.js
@@ -0,0 +1,19 @@
+export function NotFoundPage() {
+
+    return `
+
+        <section class="not-found">
+
+            <div class="container">
+
+                <h1>
+                    404
+                </h1>
+
+            </div>
+
+        </section>
+
+    `
+
+} 
