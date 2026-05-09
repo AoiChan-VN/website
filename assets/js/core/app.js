2d73e382ec3feddb6f79e0b73a76c5d393ecb657
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-09 08:17:41 +0700

    Clean up imports in app.js
    Removed unused imports from app.js

--- assets/js/core/app.js
+++ assets/js/core/app.js
@@ -1,9 +1,6 @@
 import { router } from './router.js';
-
 import { initializeTheme } from '../modules/theme.js';
-
 import { initializeAccessibility } from '../modules/accessibility.js';
-
 import { initializeInteractions } from '../modules/interaction.js';
 
 export function initApp() {
