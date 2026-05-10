f705a31fad8f7d181318ff0eca1c27b07084b2f7
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-10 15:20:16 +0700

    Delete assets directory

--- assets/js/renderer.js
+++
@@ -1,9 +0,0 @@
-import {
-    createCard
-}
-from './modules/card.js';
-
-export function renderCard(data){
-
-    return createCard(data);
-}
--- assets/js/sanitizer.js
+++
@@ -1,5 +0,0 @@
-export function text(node,value){
-
-    node.textContent =
-        value ?? '';
-}
