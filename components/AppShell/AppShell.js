76aaf53dea739a28cbf1e01c3c50dbafac59dc81
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-12 02:21:50 +0700

    Refactor AppShell to use Renderer for rendering

--- components/AppShell/AppShell.js
+++ components/AppShell/AppShell.js
@@ -1,16 +1,22 @@
 import { Component } from '../../core/Component.js';
-import { AppShellTemplate } from './AppShell.template.js';
+import { Renderer } from '../../runtime/Renderer.js';
 import { registry } from '../../runtime/Registry.js';
 
 export class AppShell extends Component {
     onMount() {
-        // Mount Taskbar vào vùng Shell
         const TaskbarClass = registry.resolveComponent('Taskbar');
-        const taskbar = new TaskbarClass();
-        taskbar.mount(this.element); 
+        new TaskbarClass().mount(this.element);
     }
 
     render() {
-        return AppShellTemplate(this.props, this.state);
+        return Renderer.createElement('div', { className: 'aoi-app-shell' },
+            Renderer.createElement('header', { className: 'aoi-shell-header' }, 
+                Renderer.createElement('span', {}, this.props.title)
+            ),
+            Renderer.createElement('div', { id: 'aoi-window-layer', className: 'aoi-window-container' }),
+            Renderer.createElement('main', { id: 'aoi-viewport', className: 'aoi-desktop' },
+                Renderer.createElement('div', { className: 'desktop-icons' }, "Aoi Runtime OS Desktop")
+            )
+        );
     }
 }
