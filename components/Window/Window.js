2bc0f03c36a488d408f68683362c830899c43ecb
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-12 02:00:49 +0700

    Refactor Window class for event handling and dragging

--- components/Window/Window.js
+++ components/Window/Window.js
@@ -5,38 +5,31 @@ import { windowManager } from '../../runtime/WindowManager.js';
 
 export class Window extends Component {
     onMount() {
-        // Cập nhật Z-Index khi có sự kiện focus
-        eventBus.on('WINDOW_FOCUS', (data) => {
-            if (data.id === this.props.id) {
-                this.element.style.zIndex = data.zIndex;
-            }
+        eventBus.on('WINDOW_FOCUS', (d) => {
+            if (d.id === this.props.id) this.element.style.zIndex = d.zIndex;
         });
-        this.#setupDragging();
+        this.#initDrag();
     }
 
-    #setupDragging() {
-        const header = this.element.querySelector('.win-header');
-        header.onmousedown = (e) => {
+    #initDrag() {
+        const h = this.element.querySelector('.win-header');
+        h.onmousedown = (e) => {
             windowManager.focus(this.props.id);
-            let pos1 = 0, pos2 = 0, pos3 = e.clientX, pos4 = e.clientY;
+            let x = e.clientX, y = e.clientY;
             document.onmousemove = (ev) => {
-                pos1 = pos3 - ev.clientX; pos2 = pos4 - ev.clientY;
-                pos3 = ev.clientX; pos4 = ev.clientY;
-                this.element.style.top = (this.element.offsetTop - pos2) + "px";
-                this.element.style.left = (this.element.offsetLeft - pos1) + "px";
+                this.element.style.left = (this.element.offsetLeft + (ev.clientX - x)) + "px";
+                this.element.style.top = (this.element.offsetTop + (ev.clientY - y)) + "px";
+                x = ev.clientX; y = ev.clientY;
             };
-            document.onmouseup = () => { document.onmousemove = null; };
+            document.onmouseup = () => document.onmousemove = null;
         };
     }
 
     destroy() { this.element.remove(); }
 
     render() {
-        return Renderer.createElement('div', { className: 'aoi-window', id: `win-${this.props.id}` },
-            Renderer.createElement('div', { className: 'win-header' }, 
-                Renderer.createElement('span', {}, this.props.title),
-                Renderer.createElement('button', { onClick: () => windowManager.close(this.props.id) }, 'X')
-            ),
+        return Renderer.createElement('div', { className: 'aoi-window', style: 'top:100px;left:100px;' },
+            Renderer.createElement('div', { className: 'win-header' }, this.props.title),
             Renderer.createElement('div', { className: 'win-body' }, this.props.content.render())
         );
     }
