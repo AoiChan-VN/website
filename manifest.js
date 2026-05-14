65902aaf195ecc232aa5c58125e1d94ef2f4c141
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-14 18:34:12 +0700

    Add manifest file for AoiChan Browser Engine

---
+++ manifest.js
@@ -0,0 +1,9 @@
+{
+    "name": "AoiChan Browser Engine",
+    "short_name": "AOI Browser",
+    "start_url": "/",
+    "display": "standalone",
+    "background_color": "#050505",
+    "theme_color": "#050505",
+    "icons": []
+} 
