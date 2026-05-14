3015d2d63c9b48a0ab440c0555f2b49d2e59b031
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-14 18:13:26 +0700

    Add bootApplication function to initialize app

---
+++ js/core/app.boot.js
@@ -0,0 +1,8 @@
+'use strict';
+
+export function bootApplication() {
+
+    document.documentElement.setAttribute('data-engine', 'aoi-browser-engine');
+
+    console.info('[AOI] Application Booted');
+} 
