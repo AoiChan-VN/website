b9e335461270d6637e14b37d4106fae84a7f7ae9
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-14 18:15:40 +0700

    Add createSidebar function for sidebar navigation

---
+++ js/ui/ui.sidebar.js
@@ -0,0 +1,22 @@
+'use strict';
+
+export function createSidebar() {
+
+    return `
+        <nav class="aoi-sidebar-nav">
+
+            <button class="aoi-sidebar-button">
+                Home
+            </button>
+
+            <button class="aoi-sidebar-button">
+                Extensions
+            </button>
+
+            <button class="aoi-sidebar-button">
+                Cookies
+            </button>
+
+        </nav>
+    `;
+} 
