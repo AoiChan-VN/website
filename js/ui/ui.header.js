e2638aeb11cea232c40ed91296ce22a197ebac3e
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-14 18:14:49 +0700

    Add createHeader function for UI header

---
+++ js/ui/ui.header.js
@@ -0,0 +1,16 @@
+'use strict';
+
+export function createHeader() {
+
+    return `
+        <div class="aoi-header-left">
+            <h1>AoiChan Browser Engine</h1>
+        </div>
+
+        <div class="aoi-header-right">
+            <button id="aoi-settings-button">
+                Settings
+            </button>
+        </div>
+    `;
+} 
