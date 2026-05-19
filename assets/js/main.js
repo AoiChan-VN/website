48c7a3375e292b9f04d0664946b6b7ccba8ec878
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-19 16:01:00 +0700

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-import { initTheme } from "./modules/theme.js";
-import { initNavbar } from "./modules/navbar.js";
-
-function bootstrap() {
-  initTheme();
-  initNavbar();
-}
-
-document.addEventListener("DOMContentLoaded", bootstrap); 
