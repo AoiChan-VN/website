8b1da94ba31bb23e7f1306be5f15eda532fa9917
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-20 16:24:28 +0700

    Add routes for Home, Projects, and Skills pages

---
+++ data/router.js
@@ -0,0 +1,16 @@
+export const routes = Object.freeze([
+    {
+        path: '/',
+        pageTitle: 'Home'
+    },
+
+    {
+        path: '/projects',
+        pageTitle: 'Projects'
+    },
+
+    {
+        path: '/skills',
+        pageTitle: 'Skills'
+    }
+]); 
