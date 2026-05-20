76d318b47ef0f01c316610e1e70507f7d02a4906
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-20 16:11:32 +0700

    Create ProjectsSection.js

---
+++ src/components/ProjectsSection.js
@@ -0,0 +1,14 @@
+export function ProjectsSection() {
+    return `
+        <section class="projects-container">
+            <header class="section-header">
+                <h2>Featured Projects</h2>
+            </header>
+
+            <div
+                class="projects-grid"
+                id="projects-grid"
+            ></div>
+        </section>
+    `;
+} 
