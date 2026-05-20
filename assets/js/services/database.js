dda7fa73435271837adf968aaaf2d4b93fa7b75e
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-20 11:30:35 +0700

    Delete assets/js/services directory

--- assets/js/services/database.js
+++
@@ -1,9 +0,0 @@
-export async function loadDatabase(path) {
-    const response = await fetch(path);
-
-    if (!response.ok) {
-        throw new Error(`Failed database: ${path}`);
-    }
-
-    return await response.json();
-}
