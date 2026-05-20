4c89ee45b8ddd96012940692154452e6e7048cd5
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-20 16:34:01 +0700

    Add runtime settings configuration

---
+++ data/settings.js
@@ -0,0 +1,8 @@
+export const runtimeSettings = Object.freeze({
+    enablePerformanceMonitor: true,
+    enableMemoryCleanup: true,
+    enableSmoothScroll: true,
+    animationThrottle: 16,
+    imageRootMargin: '200px',
+    imageThreshold: 0.01
+}); 
