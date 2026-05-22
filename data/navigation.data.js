efdba6b0ee23ac4adea7bb68ad285c2d7314432a
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-23 01:22:20 +0700

    Add navigationItems data for site navigation

---
+++ data/navigation.data.js
@@ -0,0 +1,38 @@
+const navigationItems = Object.freeze([
+    {
+        id: "home",
+        label: "Trang chủ",
+        href: "./index.html",
+        external: false
+    },
+
+    {
+        id: "products",
+        label: "Sản phẩm",
+        href: "./pages/products.html",
+        external: false
+    },
+
+    {
+        id: "docs",
+        label: "Tài liệu",
+        href: "./pages/docs.html",
+        external: false
+    },
+
+    {
+        id: "about",
+        label: "Giới thiệu",
+        href: "./pages/about.html",
+        external: false
+    },
+
+    {
+        id: "contact",
+        label: "Liên hệ",
+        href: "./pages/contact.html",
+        external: false
+    }
+]);
+
+export default navigationItems; 
