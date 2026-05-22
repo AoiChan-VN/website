1d79ddef975daf767d275dfa17eae9e8af8f7b7f
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-22 20:21:38 +0700

    Delete data directory

--- data/settings.js
+++
@@ -1,8 +0,0 @@
-export const runtimeSettings = Object.freeze({
-    enablePerformanceMonitor: true,
-    enableMemoryCleanup: true,
-    enableSmoothScroll: true,
-    animationThrottle: 16,
-    imageRootMargin: '200px',
-    imageThreshold: 0.01
-}); 
