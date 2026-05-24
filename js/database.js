9fbc7d399241d0239cea71dfc9dc7749b6a30270
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-24 14:04:14 +0700

    Create database.js

---
+++ js/database.js
@@ -0,0 +1,31 @@
+export class DatabaseEngine {
+    constructor() {
+        this.treePath = 'database/aoi_tree.json';
+        this.folderBasePath = 'database';
+    }
+
+    async fetchTree() {
+        const response = await fetch(this.treePath);
+        return await response.json();
+    }
+
+    async loadFolderData(folderName, fileName = 'aoi_plugins.json') {
+        const response = await fetch(`${this.folderBasePath}/${folderName}/${fileName}`);
+        return await response.json();
+    }
+
+    async fetchAllProducts() {
+        const tree = await this.fetchTree();
+        let aggregateData = [];
+        for (const item of tree) {
+            try {
+                const data = await this.loadFolderData(item.folder);
+                aggregateData = [...aggregateData, ...data];
+            } catch (error) {
+                console.error(`Error mapping folder: ${item.folder}`, error);
+            }
+        }
+        return aggregateData;
+    }
+}
+ 
