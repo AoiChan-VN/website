47fad14bf2e3a2e644c1dc35833a8c335231dc7b
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-24 14:07:33 +0700

    Create panel.js

---
+++ js/components/panel.js
@@ -0,0 +1,17 @@
+import { CardComponent } from './card.js';
+
+export class PanelComponent {
+    constructor(targetContainerId, onItemSelect) {
+        this.container = document.getElementById(targetContainerId);
+        this.onItemSelect = onItemSelect;
+    }
+
+    renderGrid(products) {
+        this.container.innerHTML = '';
+        products.forEach(product => {
+            const card = new CardComponent(product, this.onItemSelect);
+            this.container.appendChild(card.render());
+        });
+    }
+}
+ 
