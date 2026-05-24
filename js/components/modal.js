743ec037df7a207fd598526e6c7c90536693339c
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-24 14:05:56 +0700

    Create modal.js

---
+++ js/components/modal.js
@@ -0,0 +1,31 @@
+export class ModalComponent {
+    constructor(containerId) {
+        this.container = document.getElementById(containerId);
+    }
+
+    render(data) {
+        this.container.innerHTML = `
+            <div class="modal-content">
+                <button class="close-modal" id="close-modal-btn">✕</button>
+                <h2>${data.name}</h2>
+                <br>
+                <p><strong>ID Sản phẩm:</strong> ${data.id}</p>
+                <p><strong>Định dạng File cấu hình:</strong> ${data.file}</p>
+                <p><strong>Nguồn phát hành:</strong> ${data.link}</p>
+            </div>
+         airs`;
+        
+        this.container.classList.add('active');
+        
+        document.getElementById('close-modal-btn').addEventListener('click', () => this.close());
+        this.container.addEventListener('click', (e) => {
+            if(e.target === this.container) this.close();
+        });
+    }
+
+    close() {
+        this.container.classList.remove('active');
+        this.container.innerHTML = '';
+    }
+}
+ 
