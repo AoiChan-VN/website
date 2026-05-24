9ee41a6d99cf0d828ca47a456985a92d6b2ea27d
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-24 10:14:38 +0700

    Create config.js

---
+++ core/config.js
@@ -0,0 +1,12 @@
+export const AOI_CONFIG = Object.freeze({
+  appName: 'AOI CORE',
+
+  paths: {
+    tree: './data/aoi_tree.json',
+    folder: './data/'
+  },
+
+  selectors: {
+    app: '#app'
+  }
+}); 
