cac0cd225e8550db7281645554bd58d90c140a58
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-26 00:21:15 +0700

    Create cards.js

---
+++ assets/js/ui/cards.js
@@ -0,0 +1,43 @@
+export function createCard(item) {
+
+  const article =
+    document.createElement("article");
+
+  article.className = "aoi-card";
+
+  article.innerHTML = `
+    <div class="card-image">
+      <img
+        src="${item.img}"
+        alt="${item.name}"
+        loading="lazy"
+      />
+    </div>
+
+    <div class="card-content">
+
+      <h2 class="card-title">
+        ${item.name}
+      </h2>
+
+      <p class="card-description">
+        ${item.description}
+      </p>
+
+      <div class="card-actions">
+
+        <button
+          class="action-button"
+          data-action="panel"
+          type="button"
+        >
+          •••
+        </button>
+
+      </div>
+
+    </div>
+  `;
+
+  return article;
+} 
