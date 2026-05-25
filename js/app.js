fec7cdfd5fbec5e8ca4e81d6e82920b859990b2a
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-25 18:49:59 +0700

    Delete js directory

--- js/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    if (window.loadProjectData && window.renderCards) {
-        const data = await window.loadProjectData();
-        window.renderCards(data);
-    } else {
-        console.error("Hệ thống core script bị thiếu hoặc tải sai thứ tự.");
-    }
-});
