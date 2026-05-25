95d048bfedec17161f8ec34f123f6c2c71af06a8
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-25 20:41:17 +0700

    Create render-loading.js

---
+++ assets/js/modules/render-loading.js
@@ -0,0 +1,15 @@
+export function renderLoading(container) {
+
+  container.innerHTML = `
+    <div class="loading-state glass-card">
+
+      <div class="loading-spinner"></div>
+
+      <p class="loading-text">
+        Loading Portfolio...
+      </p>
+
+    </div>
+  `;
+
+} 
