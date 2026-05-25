a1e415ce0eb620655c515d4d98bcbf3ef6cefe9f
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-25 19:15:27 +0700

    Create render-empty.js

---
+++ assets/js/modules/render-empty.js
@@ -0,0 +1,17 @@
+export function renderEmpty(container) {
+
+  container.innerHTML = `
+    <div class="empty-state glass-card">
+
+      <h3 class="empty-title">
+        Empty Portfolio
+      </h3>
+
+      <p class="empty-description">
+        Không có dữ liệu để hiển thị.
+      </p>
+
+    </div>
+  `;
+
+} 
