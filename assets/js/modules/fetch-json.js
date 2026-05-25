e97cb6ce848c010b527ebd911ea55386af77746b
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-25 20:28:37 +0700

    Update fetch-json.js

--- assets/js/modules/fetch-json.js
+++ assets/js/modules/fetch-json.js
@@ -1,10 +1,18 @@
 export async function fetchJson(path) {
 
-  const response = await fetch(path);
+  const response =
+    await fetch(path, {
+      cache: "no-store"
+    });
 
   if (!response.ok) {
-    throw new Error(`Failed to fetch: ${path}`);
+
+    throw new Error(
+      `Failed to fetch: ${path}`
+    );
+
   }
 
   return await response.json();
-} 
+
+}
