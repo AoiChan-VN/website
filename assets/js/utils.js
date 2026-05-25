25822e46b851408e992a4a3fb6df64b13b870afd
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-25 15:03:11 +0700

    Create utils.js

---
+++ assets/js/utils.js
@@ -0,0 +1,10 @@
+export function createElement(tag, className = ""){
+
+  const element = document.createElement(tag);
+
+  if(className){
+    element.className = className;
+  }
+
+  return element;
+} 
