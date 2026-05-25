ed2424e6623852856a32e554f41d965215899471
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-25 15:05:19 +0700

    Create markdown.js

---
+++ assets/js/markdown.js
@@ -0,0 +1,7 @@
+export function parseMarkdown(markdown){
+
+  return markdown
+    .replace(/^# (.*$)/gim, "<h1>$1</h1>")
+    .replace(/^## (.*$)/gim, "<h2>$1</h2>")
+    .replace(/\n/g, "<br>");
+} 
