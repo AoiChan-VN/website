5360e08369080e512ef547b9bce1c4c697e57604
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-26 23:42:59 +0700

    Update bootstrap.js

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -1,11 +1,22 @@
-import { initRouter } from "./router/router.js";
-import { initOverlay } from "./modules/overlay.js";
-import { initMotion } from "./modules/motion.js";
+import { initRouter }
+from "./router/router.js";
+
+import { initOverlay }
+from "./modules/overlay.js";
+
+import { initMotion }
+from "./modules/motion.js";
+
+import { initBackgroundFX }
+from "./modules/background.js";
 
 async function bootstrap() {
 
   initOverlay();
+
   initMotion();
+
+  initBackgroundFX();
 
   await initRouter();
 
