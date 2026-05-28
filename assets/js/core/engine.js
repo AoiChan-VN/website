1f6678800970b4432be590abb2ad540eb693d069
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-28 14:52:54 +0700

    Update engine.js

--- assets/js/core/engine.js
+++ assets/js/core/engine.js
@@ -2,11 +2,19 @@ import { initializeParallax } from '../modules/parallax.js';
 import { initializeViewport } from '../modules/viewport.js';
 import { initializePerformance } from './performance.js';
 
+let initialized = false;
+
 export function initializeEngine(){
+
+  if(initialized){
+    return;
+  }
+
+  initialized = true;
 
   initializeViewport();
 
   initializePerformance();
 
   initializeParallax();
-} 
+}
