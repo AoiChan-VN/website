2d1b358d3e1ff1cad252858099f51a1d7c96c3af
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-28 14:50:56 +0700

    Delete assets/js/utils directory

--- assets/js/utils/dom.js
+++
@@ -1,9 +0,0 @@
-export function select(selector){
-  return document.querySelector(selector);
-}
-
-export function selectAll(selector){
-  return [
-    ...document.querySelectorAll(selector)
-  ];
-} 
