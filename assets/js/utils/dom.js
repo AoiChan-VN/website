f83475e1fba18ee1487347b2630470a1a9d49838
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-28 14:10:21 +0700

    Create dom.js

---
+++ assets/js/utils/dom.js
@@ -0,0 +1,9 @@
+export function select(selector){
+  return document.querySelector(selector);
+}
+
+export function selectAll(selector){
+  return [
+    ...document.querySelectorAll(selector)
+  ];
+} 
