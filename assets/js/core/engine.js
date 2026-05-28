67a46a1d1b53b4fb6f4f0b5e9333a4627bf16ad3
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-28 18:32:00 +0700

    Update engine.js

--- assets/js/core/engine.js
+++ assets/js/core/engine.js
@@ -4,6 +4,9 @@ import { initializeParticles } from '../modules/particles.js';
 import { initializeCursor } from '../modules/cursor.js';
 import { initializeReveal } from '../modules/reveal.js';
 import { initializeScrollDepth } from '../modules/scroll-depth.js';
+import { initializeLoading } from '../modules/loading.js';
+import { initializeMobileMenu } from '../modules/mobile-menu.js';
+import { initializeLazyMedia } from '../modules/lazy-media.js';
 
 import { initializePerformance } from './performance.js';
 import { initializeFpsManager } from './fps.js';
@@ -23,6 +26,12 @@ export function initializeEngine(){
   initializePerformance();
 
   initializeFpsManager();
+
+  initializeLoading();
+
+  initializeMobileMenu();
+
+  initializeLazyMedia();
 
   initializeParallax();
 
