85e7779aab68e423d228512b84db4ee70b0d942a
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-05-28 14:09:18 +0700

    Create viewport.js

---
+++ assets/js/modules/viewport.js
@@ -0,0 +1,21 @@
+export function initializeViewport(){
+
+  const setViewportHeight = () => {
+
+    const vh =
+      window.innerHeight * 0.01;
+
+    document.documentElement.style.setProperty(
+      '--vh',
+      `${vh}px`
+    );
+  };
+
+  setViewportHeight();
+
+  window.addEventListener(
+    'resize',
+    setViewportHeight,
+    { passive:true }
+  );
+} 
